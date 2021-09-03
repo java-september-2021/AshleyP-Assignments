e9b57bd1dde0b0bae0b3372114997de10273396d
Ashley Posada <aposada26@yahoo.com>
2021-09-03 07:57:24 -0500

    updating fizzbuzz assignment

--- 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -1,5 +1,16 @@
-public class FizzBuzz{
-    public static void main(String[] args){
-        System.out.printIn("Hello World!");
+public class FizzBuzz {
+    public static String fizzBuzz(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0) {
+            return "Fizz";
+        }
+        else if (number % 5 == 0) {
+            return "Buzz";
+        }
+        else {
+            return Integer.toString(number);
+        }
     }
 }
---
+++ 01-java-fundamentals/03-fizzbuzz/TestFizzBuzz.java
@@ -0,0 +1,10 @@
+public class TestFizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz tester = new FizzBuzz();
+        System.out.println(tester.fizzBuzz(9));
+        System.out.println(tester.fizzBuzz(10));
+        System.out.println(tester.fizzBuzz(15));
+        System.out.println(tester.fizzBuzz(16));
+        System.out.println(tester.fizzBuzz(2));
+    }
+}
