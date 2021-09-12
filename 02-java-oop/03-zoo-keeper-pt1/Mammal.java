b4f5b0e5a53cbac9a01545e41905ba8ceecc8fc5
Ashley Posada <aposada26@yahoo.com>
2021-09-12 13:05:03 -0500

    zoo keeper: part 1

---
+++ 02-java-oop/03-zoo-keeper-pt1/Mammal.java
@@ -0,0 +1,12 @@
+public class Mammal {
+    protected int energyLevel = 100;
+
+    public int displayEnergy() {
+        System.out.println("Energy level: " + energyLevel);
+        return energyLevel;
+    }
+    
+    public Mammal(int energyLevel) {
+        this.energyLevel = energyLevel;
+    }
+}
