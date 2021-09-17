7a4ac438911c794ecc7a8c9a2a5478d1a9840ab1
Ashley Posada <aposada26@yahoo.com>
2021-09-17 13:55:24 -0500

    getting familiar with routing in spring boot

---
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/src/main/java/com/ashley/gettingfamiliarwithrouting/controllers/DojoController.java
@@ -0,0 +1,24 @@
+package com.ashley.gettingfamiliarwithrouting.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DojoController {
+	@RequestMapping("/dojo")
+	public String dojo() {
+		return "The dojo is awesome!";
+	}
+	@RequestMapping("/dojo/{location}")
+	public String dojolocation(@PathVariable("location") String location) {
+		switch(location) {
+		case "burbank":
+			return "Burbank Dojo is located in Southern California";
+		case "san-jose":
+			return "SJ dojo is the headquarters";
+		default:
+			return String.format("%s is awesome!", location);
+		}
+	}
+}
